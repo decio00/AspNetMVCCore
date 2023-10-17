700d67c02ee66f8924f97b6d71ed9cdbe8c24b3c
Oleksiy Franchuk <o.franchuk@datamate.it>
2023-10-17 16:52:39 +0200

    17-10-2023

---
+++ AspNetMVCCore/Controllers/CalcolatriceController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetMVCCore.Controllers
+{
+    public class CalcolatriceController : Controller
+    {
+        [HttpGet]
+        public IActionResult Calcolatrice()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ActionName("Calcolatrice")]
+        public IActionResult CalcolatricePost()
+        {
+
+            double.TryParse(HttpContext.Request.Form["txt7"], out double n1);
+            double.TryParse(HttpContext.Request.Form["txt8"].ToString(), out double n2);
+            ViewData["Num1"] = n1;
+            ViewBag.Num2 = n2;
+            ViewBag.Risultato = n1 + n2;
+
+            return View();
+        }
+
+    }
+}
---
+++ AspNetMVCCore/Controllers/ProvaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetMVCCore.Controllers
+{
+    public class ProvaController : Controller
+    {
+        public IActionResult Pagina1()
+        {
+            return View();
+        }
+    }
+}
