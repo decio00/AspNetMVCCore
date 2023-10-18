ec8ea8fa8a88b1bd1903ac5b16f8a5eaf771676a
Oleksiy Franchuk <o.franchuk@datamate.it>
2023-10-18 17:14:35 +0200

    finale 18-10

--- AspNetMVCCore/Controllers/ClientiController.cs
+++ AspNetMVCCore/Controllers/ClientiController.cs
@@ -43,5 +43,27 @@ namespace AspNetMVCCore.Controllers
             var query1 = _northwindContext.Customers.Where(c => c.Country == paese).ToList();
             return View(query1);
         }
+        public IActionResult ClientiComboTag(string paese)
+        {
+            var query = _northwindContext.Customers.GroupBy(c => c.Country).Select(k => k.Key).ToList();
+            ViewBag.Combo = new Microsoft.AspNetCore.Mvc.Rendering.SelectList(query, paese);
+
+            var query1 = _northwindContext.Customers.Where(c => c.Country == paese).ToList();
+            return View(query1);
+        }
+        public IActionResult ClientiOrdini()
+        {
+            //var query = _northwindContext.Join(_northwindContext.Orders, _northwindContext.Customers, )
+
+            var query = _northwindContext.Customers
+                .Join(_northwindContext.Orders,
+                    c => c.CustomerId,
+                    o => o.CustomerId,
+                    (c, o) => new ClientiOrdini { OrderId =  o.OrderId, OrderDate =  o.OrderDate.Value, CompanyName = c.CompanyName, City = c.City, Country = c.Country })
+                .Select(result => result);
+
+
+            return View(query.ToList());
+        }
     }
 }
---
+++ AspNetMVCCore/Models/ClientiOrdini.cs
@@ -0,0 +1,11 @@
+﻿namespace AspNetMVCCore.Models
+{
+    public class ClientiOrdini
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string CompanyName { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
